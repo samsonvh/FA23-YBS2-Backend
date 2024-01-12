a74396d8fd10e4b5bf32567532f0ebb17599422f
SamsonVH <samson.vh.20@gmail.com>
2024-01-12 19:27:46 +0700

    Add FCM and change Exception Response

---
+++ YBS2.Service/Dtos/Details/FCMMessageRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YBS2.Service.Dtos.Details
+{
+    public class FCMMessageRequest
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string DeviceToken { get; set; }
+    }
+}
---
+++ YBS2.Service/Services/IFirebaseCMService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YBS2.Service.Dtos.Details;
+
+namespace YBS2.Service.Services
+{
+    public interface IFirebaseCMService
+    {
+        Task<bool> SendTransactionNotification(FCMMessageRequest request);
+    }
+}
