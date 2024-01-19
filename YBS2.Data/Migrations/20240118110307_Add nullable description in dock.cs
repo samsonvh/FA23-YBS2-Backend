5fae2fad273040d735123eb11e4259b717601aa6
KhangNNDev <Khanga123456789@gmail.com>
2024-01-19 15:57:22 +0700

    Add Filter tour

---
+++ YBS2.Data/Migrations/20240118110307_Add nullable description in dock.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YBS2.Data.Migrations
+{
+    public partial class Addnullabledescriptionindock : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Dock",
+                type: "nvarchar(500)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Dock",
+                type: "nvarchar(500)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldNullable: true);
+        }
+    }
+}
